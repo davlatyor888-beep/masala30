9c199a660963693ef0b6159ba6c2366577d3dd71
davlatyor888-beep <davlatyor888@gmail.com>
2025-11-03 18:38:59 +0500

    Add files via upload

---
+++ davlatyor1.py
@@ -0,0 +1,296 @@
+#1-masala
+# fruits = ['olma', 'banan', 'uzum']
+# print(fruits)
+# fruits.append ('nok')
+# print("nok ohiriga qoshildi", fruits )
+# fruits2=len(fruits)
+# print("royhatning uzunligi",fruits2, fruits)
+# if 'olma' in fruits:
+#  print("royhatda olma borligi" )
+# else:
+#     print("olma royhatta mavjud emasligi")
+#
+# f=fruits.index( 'banan')
+# print("banan indexsi",f)
+
+#2-masala
+# colors = ['qizil', 'yashil', 'ko\'k']
+# print(colors)
+# colors.append('sariq')
+# print("royhattda sariq borligi", colors)
+#
+# colors.sort()
+# print("royhat alifbo tartibida", colors )
+#
+# colors2=len(colors)
+# print("Ro'yxatning uzunligi", colors2, colors)
+# if 'yashil' in colors:
+#  print("royhatda yashil borligi" )
+# else:
+#     print("yashil royhatta mavjud emasligi")
+
+#3-masALA
+# cities = ['Toshkent', 'Samarqand', 'Buxoro']
+# print(cities)
+# cities.append('Xiva')
+# print("Ro'yxatda xiva borligi ", cities)
+# citi = cities.index('Samarqand')
+# print("samarandning indeksi", citi)
+#
+# c= len(cities)
+# print("royhtning uzunligi", c)
+#
+# cities.sort()
+# print("royhat alifbo tartibida joylashuvi", cities)
+
+#4-misol
+
+# numbers = [5, 2, 8, 1]
+# print(numbers)
+# numbers.append(10)
+# print("royhtda 10 borligi", numbers)
+# numbers.sort()
+# print("royhat osih tatibid joylashihi", numbers)
+# numbers2 = len(numbers)
+# print("royhtning uzunligi", numbers2)
+# if 8 in numbers:
+#  print("royhatda 8 borligi" )
+# else:
+#     print("8 royhatta mavjud emasligi")
+
+#5- misol
+# animals = ['mushuk', 'it', 'qush']
+# print(animals)
+# animals.append('baliq')
+# print("royhtt baliq brligi", animals)
+# animals.sort()
+# print("royhat alifbo tartibida joylashishi", animals)
+# a = animals.index('it')
+# print("itning indeksi", a)
+#
+# animals2= len(animals)
+# print("royhatning uzunligi", animals2)
+
+#6-misol
+# fruitss = ['olma', 'banan', 'uzum', 'nok']
+# print(fruitss)
+# fruitss.append('kivi')
+# print("royhatta kivi borligi", fruitss)
+# fruitss.remove('banan')
+# print("royhatta bananni olib tashlash", fruitss)
+# fruitss.insert(2, 'shaftoli')
+# print("royhatta shftoli borligi", fruitss)
+# fruitss.sort()
+# print("royhat alifbo tartibida", fruitss)
+# fruitss2= len(fruitss)
+# print("royhat uzunligi", fruitss2)
+# fruitss.index('olma')
+# print("royhatta olma borligi", fruitss)
+# if 'kivi' in fruitss:
+#  print("royhatda kivi borligi" )
+# else:
+#     print("kivi royhatta mavjud emasligi")
+# fruitss.sort(reverse=True)
+# print('royhat tskarisi',fruitss)
+# fruitss.copy()
+# print('royhat copysi', fruitss)
+
+#7-misol
+# menu = ['osh', "lagmon", 'shashlik', 'somsa']
+# print(menu)
+# menu.extend(['manti', 'norin'])
+# print('mati va norin borligi', menu)
+# menu.remove('lagmon')
+# print("lagmon olib tashlanganligi", menu)
+# menu.sort()
+# print("alifbo trtibida",menu)
+# menu.pop()
+# print('ogirgi narsani ochiradi', menu)
+# menu2 = len(menu)
+# print("uzunligi", menu2)
+# menu.index('osh')
+# print("ohning indexi", menu)
+# menu.count('somsa')
+# print("nechta somsa borligi", menu)
+#
+# if 'manti' in menu:
+#  print("royhatda mantiborligi" )
+# else:
+#     print("manti royhatta mavjud emasligi")
+# menu.copy()
+# print('nusxalashi', menu)
+
+#8-misol
+# citiess = ['Toshkent', 'Samarqand', 'Buxoro', 'Xiva']
+# print(citiess)
+# citiess.insert(0, 'andijon')
+# print('andijon qoshilihi', citiess)
+# citiess.remove('Xiva')
+# print('xiva yoqolishi', citiess)
+# d=citiess.index('Samarqand')
+# print('samarqand indxi', d)
+# citiess.sort()
+# print("alifbo tartibi", citiess)
+# citiess2 = len(citiess)
+# print('uznligi', citiess2)
+# if 'Buxoro' in citiess:
+#  print("royhatda buxora borligi" )
+# else:
+#     print("buxoro royhatta mavjud emasligi")
+# citiess.pop()
+# print('oxirgi shaar ochirilgan', citiess)
+# citiess.sort(reverse=True)
+# print('teskari royhat', citiess)
+# citiess.copy()
+# print('nusxa', citiess)
+
+#9-misol
+# students = ['Ali', 'Vali', 'Sardor', 'Aziz']
+# print(students)
+# students.append('jamshet')
+# print('jamshet qoshilgn', students)
+# students.remove('Sardor')
+# print('sardor olib tashlandi', students)
+# students.insert(1, 'Diyor')
+# print('vali va ali qoshilgan', students)
+# students.sort()
+# print('alifbo tartibi', students)
+# s=students.index('Aziz')
+# print(f)
+# if 'Diyor' in students:
+#  print("royhatda diyor borligi" )
+# else:
+#     print("diyor royhatta mavjud emasligi")
+# students.pop()
+# print('oxirgi talaba olib tashlandi', students)
+# students2= len(students)
+# print('uzunligi', students2)
+# students.copy()
+# print('nusxasi', students)
+
+#10-m
+# favorites = ['telefon', 'noutbuk', 'naushnik']
+# print(favorites)
+# favorites.append('planshet')
+# print('plansheet qoshildi', favorites)
+# favorites.append('soat')
+# print('soat qoshildi', favorites)
+# favorites.sort()
+# print('alifbo tartibida', favorites)
+# favorites2 = len(favorites)
+# print('uzunligi', favorites2)
+# f=favorites.index('noutbuk')
+# print('noutbukning indexi', f)
+# if 'telefon' in favorites:
+#  print("royhatda telefon borligi" )
+# else:
+#     print("telefon royhatta mavjud emasligi")
+# favorites.remove('naushnik')
+# print('noushnik olib tashlandi ', favorites)
+# favorites.pop()
+# print('oxirgimaxsulot ochirilgan', favorites)
+# favorites.copy()
+# print('nusxasi', favorites)
+
+
+
+#11-m
+# songs = ['Yomgir', 'Sevgi', 'Hayot', 'Kuz']
+# print(songs)
+# songs.insert(1,'Sevgi')
+# print('Sevgi dan oldin Bahor', songs)
+# songs.remove('Kuz')
+# print('kuz ochirib tashlandi', songs)
+# songs.sort()
+# print('alifbo tartibida', songs)
+# h=songs.index('Yomgir')
+# print('yomgirning indexi', h)
+# if 'Hayot' in songs:
+#  print("royhatda hayot borligi" )
+# else:
+#     print("hayot royhatta mavjud emasligi")
+# songs.pop()
+# print('oxirgi element ochirildi', songs)
+# song=len (songs)
+# print('uzunligi', song)
+# songs.sort(reverse=True)
+# print('tskrisi', songs)
+# l = songs.copy()
+# print('nusxasi', l)
+# 12-m
+# books = ['Alximik', 'Shaytanat', '1984']
+# print(books)
+# l = ['otkan kunlar', "sariq devni minib"]
+# books.extend(l)
+# print(books)
+# books.remove('Shaytanat')
+# print(books)
+# books.sort()
+# print(books)
+# n=books.index('1984')
+# print(n)
+# if 'Alimxon' in books:
+#  print("True" )
+# else:
+#     print("False")
+# books.pop()
+# print(books)
+# book = len(books)
+# print(book)
+# print(books.sort(reverse=True))
+# h = books.copy()
+# print(h)
+#13-m
+# movies = ['Inception', 'Matrix', 'Interstellar']
+# print(movies)
+# movies.append('parasite')
+# print(movies)
+# movies.remove('Matrix')
+# print(movies)
+# movies.sort()
+# print(movies)
+# k=movies.index('Inception')
+# print(k)
+# if 'Intersteller' in movies:
+#  print("True" )
+# else:
+#     print("False")
+# movies.pop()
+# print(movies)
+# book = len(movies)
+# print(book)
+# print(movies.sort(reverse=True))
+# h = movies.copy()
+# print(h)
+#14-m
+# emails = ['job_offer', 'spam', 'friend', 'news']
+# print(emails)
+# emails.append('famiil')
+# print(emails)
+#
+# emails.remove('spam')
+# print(emails)
+# emails.sort()
+# print(emails)
+# o = emails.index('friend')
+# print(o)
+# if 'job_offer' in emails:
+#  print("True" )
+# else:
+#     print("False")
+# emails.pop()
+# print(emails)
+# b = len(emails)
+# print(b)
+# print(emails.sort(reverse=True))
+# h = emails.copy()
+# print(h)
+
+
+
+
+
+
+
+
+
